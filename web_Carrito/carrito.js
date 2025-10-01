e06fcc07df5cc47151f106497cffd5b20266f06a
COMPUS021\daw2 <adrianhoyos098@gmail.com>
2025-10-01 18:36:12 +0200

    a

--- web_Carrito/carrito.js
+++ web_Carrito/carrito.js
@@ -5,8 +5,35 @@ export class Carrito {
         this.currency = "€";
         this.productos = [];
     }
+
+
+// Método que actualiza o añade productos
+actualizarUnidades(sku, unidades) {
+    // Buscamos si el producto ya existe en el carrito
+    const item = this.items.find(function(p) {
+        return p.sku === sku;
+    });
+
+    if (item) {
+        // Si ya existe → cambiamos la cantidad
+        item.quantity = unidades;
+    } else {
+        // Si no existe → lo añadimos
+        this.productos.push({ sku, quantity: unidades });
+    }
 }
 
-actualizarUnidades (sku, unidades) {
-    
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//
