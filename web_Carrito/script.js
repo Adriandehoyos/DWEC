9c48a975f82e2bcd91c25abd508890a49651a932
COMPUS021\daw2 <adrianhoyos098@gmail.com>
2025-10-03 18:25:31 +0200

    no message

--- web_Carrito/script.js
+++ web_Carrito/script.js
@@ -11,20 +11,40 @@ document.addEventListener('DOMContentLoaded', function () {
     const bMenos = document.createElement('button');
     const cajitaCant = document.createElement('Input');
     
+    bMas.textContent = '+';
+    bMenos.textContent = '-';
+    cajitaCant.type = 'number'
 
+// Crear filas con map
+const filas = data.products.map(function (x) {
+    const tr = document.createElement("tr");
 
-    
+    // Columna 1: título + SKU
+    const td1 = document.createElement("td");
+    td1.textContent = `${x.title} <br> ref: ${x.SKU}`; //arreglar esto 
+    tr.append(td1);
 
-    tablebody.innerHTML = data.products.map(function(x){
-        return`
-        <tr>
-            <td>${x.title} <br> ref: ${x.SKU}</td>
-            <td>${bMenos}${cajitaCant}${bMas}</td> 
-            <td>${x.price}${data.currency}</td>
-            <td>${data.currency}</td>
-        </tr>
-        `
-        }).join("");//Este join es para poder usar la comilla ` en el map y luego no se me muestre en la tabla.
+    // Columna 2: botones y cantidad
+    const td2 = document.createElement("td");
+    td2.append()//Tengo que meter los botones todavia
+    tr.append(td2);
+
+    // Columna 3: precio
+    const td3 = document.createElement("td");
+    td3.textContent = `${x.price}${data.currency}`;
+    tr.append(td3);
+
+    // Columna 4: moneda
+    const td4 = document.createElement("td");
+    td4.textContent = data.currency;
+    tr.append(td4);
+
+    return tr; // devolvemos el <tr>
+});
+
+// Agregar todas las filas de golpe
+tablebody.append(...filas);
+
 
 
 
