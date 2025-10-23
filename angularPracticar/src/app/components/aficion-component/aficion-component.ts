0ebdf74771549f541ce2ca80f1caf3290d7a3772
COMPUS021\daw2 <adrianhoyos098@gmail.com>
2025-10-23 18:37:05 +0200

    empezando angular

---
+++ angularPracticar/src/app/components/aficion-component/aficion-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aficion-component',
+  imports: [],
+  templateUrl: './aficion-component.html',
+  styleUrl: './aficion-component.css',
+})
+export class AficionComponent {
+
+}
