79e9ab2d35e07ab206cf3705c162ae46c688685a
COMPUS021\daw2 <adrianhoyos098@gmail.com>
2025-11-27 17:38:16 +0100

    avances en clase

--- SimulacroExamen/src/app/app.routes.ts
+++ SimulacroExamen/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { ProductosComponent } from './pages/productos/productos.component';
+import { SobrenosotrosComponent } from './pages/sobrenosotros/sobrenosotros.component';
+import { FormularioComponent } from './pages/formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'inicio'},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'productos', component: ProductosComponent},
+  {path: 'sobrenosotros', component: SobrenosotrosComponent},
+  {path: 'formulario', component: FormularioComponent},
+  {path: '**', redirectTo: 'inicio'}
+
+];
